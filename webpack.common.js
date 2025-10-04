df3f2bef34de84349517d602d9c5d0c320bdf865
Elina test <elina.ulli93@gmail.com>
2025-10-04 14:00:42 +0500

    Устрание проблем с action

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,9 @@ module.exports = {
   target: "web",
   output: {
     path: path.resolve(__dirname, "dist"),
-    publicPath: "",
+    filename: 'bundle.[contenthash].js',
+    clean: true,
+    publicPath: "/dom-event/",
   },
   module: {
     rules: [
